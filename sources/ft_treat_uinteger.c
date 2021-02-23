40eee8b5fe8ada8b9099aac686934446307864ee
dinosama <apatoutaaron.apatout@gmail.com>
2021-02-23 04:58:51 +0100

    adnoi

--- sources/ft_treat_uinteger.c
+++ sources/ft_treat_uinteger.c
@@ -16,10 +16,7 @@ int	ft_treat_uinteger(t_flags *flags, unsigned int n)
 {
 	int	len;
 	int	zero;
-	int	final_lenght;
 
-	if (n == 0)
-		final_lenght = 1;
 	zero = 0;
 	n = n % 4294967296;
 	len = countnumbers((unsigned long long)n, 10);
@@ -35,6 +32,6 @@ int	ft_treat_uinteger(t_flags *flags, unsigned int n)
 	if (flags->minus == 1)
 		ft_treat_width(*flags, len);
 	if (flags->width > len)
-		return (len + (flags->width - len) + final_lenght);
-	return (len + final_lenght);
+		return (len + (flags->width - len));
+	return (len);
 }
